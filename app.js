375c49981a61a626e8284a8071ac280744b2a976
Heather Lamkins <lamkinsheather@gmail.com>
2020-12-10 19:30:10 -0600

    Updated files

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-let employees = [];
+
 const employees = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
